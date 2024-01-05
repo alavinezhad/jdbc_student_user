ef86654d5deda705e3ef1a7cdb7b01bf90aaa5f5
alavinezhad <git.hassan.alavi@gmail.com>
2024-01-05 16:23:26 +0330

    add connection to jdbc

--- src/main/java/JdbcConnection.java
+++ src/main/java/JdbcConnection.java
@@ -1,2 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+    Connection connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/postgres",
+            "postgres",
+            "67065018"
+    );
+
+    public JdbcConnection() throws SQLException {
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }

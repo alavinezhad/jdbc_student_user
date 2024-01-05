fad8cbb011d2f8e885e35853d99ca1526b14acdd
alavinezhad <git.hassan.alavi@gmail.com>
2024-01-05 16:33:20 +0330

    add constructors to User class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -7,5 +7,19 @@ public class User {
     private String password;
     private Date signUp_date;
 
+    public User() {
+    }
 
+    public User(String username, String password, Date signUp_date) {
+        this.username = username;
+        this.password = password;
+        this.signUp_date = signUp_date;
+    }
+
+    public User(int id, String username, String password, Date signUp_date) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.signUp_date = signUp_date;
+    }
 }

5190bd1f05d644d35b4b98587ab614ef0eb29d7e
alavinezhad <git.hassan.alavi@gmail.com>
2024-01-05 16:31:33 +0330

    add fields to User class

---
+++ src/main/java/User.java
@@ -0,0 +1,11 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private Date signUp_date;
+
+
+}
